8cd957eeb73cb55573398b097d7f44a82f603158
aman_sama <amanrai2002acr@gmail.com>
2025-08-22 12:46:02 +0545

    deployment fixes

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ const App = () => {
   //default city as tokyo
   useEffect(() => {
     const defaultCity = "Tokyo";
-    const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${defaultCity}&days=2`;
+    const API_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${defaultCity}&days=2`;
     getWeatherDetails(API_URL);
   }, []);
 
